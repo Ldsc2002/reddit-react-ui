3eb6eae2405738fa40dd09fd70898e4d4c830c7c
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-16 17:36:02 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import NavigationBar from './Components/Navbar'
+
+function App() {
+    return (
+        <div>
+            {/* <NavigationBar /> */}
+
+            <p>This is a test</p>
+        </div>
+    )
+}
+
+export default App
