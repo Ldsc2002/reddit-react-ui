c53343bc24b55d9157d34b6804c1eda04eae9149
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-16 21:14:03 -0600

    Show information in cards

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 function Post( {data} ) {
 
     return (
-        <p>{data.title}</p>
+        <div className='post-card'>
+            <div className='sidebar-card'>
+                <p>P</p>
+            </div>
+            <div className='card-content'>
+                <div className='card-top'>
+                    <p className='inline subreddit-name'>{data.subreddit_name_prefixed}</p>
+                    <p className='inline author-name'>Posted by u/{data.author}</p>
+                    <button className='inline join-btn'>Join</button>
+                </div>
+                
+                <p className='post-title'>{data.title}</p>
+            </div>
+        </div>
     )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './style/posts.css'
+import './style/app.css'
+
 
 ReactDOM.render(
     <App />,
