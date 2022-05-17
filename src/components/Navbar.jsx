3ae7453ade872e44adb99a3cf9f2758bcc34f479
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-16 22:33:34 -0600

    WIP top navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,43 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
 import Container from 'react-bootstrap/Container'
 
 function NavigationBar() {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" fixed='top'>
+            <Navbar.Brand href="#">
+                <img
+                alt=""
+                src="./src/images/logo.svg"
+                height="32"
+                className="d-inline-block align-top"
+                />{' '}
+            </Navbar.Brand>
+                
             <Container>
-                <Navbar.Brand href="#home">
-                    <img
-                    alt=""
-                    src="./src/images/logo.svg"
-                    height="40"
-                    className="d-inline-block align-top"
-                    />{' '}
-                </Navbar.Brand>
+                <Form className="d-flex">
+                    <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    />
+                </Form>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="#">Home</Nav.Link>
+                        <Nav.Link href="#">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+// CSS
 import './style/posts.css'
 import './style/app.css'
-
+import './style/navbar.css'
 
 ReactDOM.render(
     <App />,
