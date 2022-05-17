6375810e2f534de5a4f794d4cd6de04c8271d206
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-16 19:49:25 -0600

    Changed navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,14 @@ function NavigationBar() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img
+                    alt=""
+                    src="./src/images/logo.svg"
+                    height="40"
+                    className="d-inline-block align-top"
+                    />{' '}
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
