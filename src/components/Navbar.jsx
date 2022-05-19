365dc043b99ca76835af64068a660953785c73a4
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-18 18:39:11 -0600

    Added navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,31 +19,19 @@ function NavigationBar() {
                 {' '}
             </Navbar.Brand>
 
-            <Container>
-                <Form className="d-flex">
+            <div className='nav-container'>
+                <Form className="d-flex search-bar">
                     <FormControl
                         type="search"
-                        placeholder="Search"
+                        placeholder="Search Reddit"
                         className="me-2"
                         aria-label="Search"
                     />
                 </Form>
 
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#">Home</Nav.Link>
-                        <Nav.Link href="#">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+                <button className='account-btn login-btn'>Log In</button>
+                <button className='account-btn signup-btn'>Sign Up</button>
+            </div>
         </Navbar>
     )
 }
